1930add38b0b9b134be3f88e1f9373031ee1694a
Panth Patel <panthhp@outlook.com>
2023-08-03 08:19:32 +0100

    load variables from environment

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,10 +1,13 @@
+import { ConfigModule } from '@nestjs/config';
 import firebase from 'firebase-admin';
+
+ConfigModule.forRoot();
 
 firebase.initializeApp({
   credential: firebase.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    projectId: process.env.FIREBASE_PROJECT_ID,
   }),
 });
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
